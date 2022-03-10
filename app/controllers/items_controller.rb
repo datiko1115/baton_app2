b4771f9d0d4aca73176e4924e6cc4b68f65607db
datiko1115 <datiko1115@gmail.com>
2022-03-10 13:02:52 +0900

    firstcommit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,72 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show]
+  before_action :ensure_correct_user, only:[:edit, :update, :destroy]
+  before_action :soldout_cant_be_edited, only:[:edit, :update]
+
+  def index
+    @items = Item.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end 
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end 
+
+  def show
+  end 
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end  
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :price, :condition_id, :recipient_id, :prefecture_id, :shipment_id, {images: []}).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def ensure_correct_user
+    unless @item.user == current_user
+     redirect_to items_path
+    end
+  end
+
+  def soldout_cant_be_edited
+    if @item.order.present?
+     redirect_to root_path
+    end
+  end
+end
---
+++ app/models/item.rb
@@ -0,0 +1,28 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_many_attached :images
+  has_one :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :shipment
+  belongs_to :recipient
+  belongs_to :condition
+
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input Half-width number' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  validates :price, numericality: { only_integer: true } 
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :recipient_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipment_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :images, presence: true
+  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
+
+
+end
